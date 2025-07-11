50aba8dd579afa890a4d0f877e3cd1878ee105c7
Anka <runner@Mac-1752266626288.local>
2025-07-11 21:31:29 +0000

    Release 0.0.1

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let url = "https://github.com/ridgelineinternational/wireguard-apple-xcframework/releases/download/0.0.0/WireGuardGoFoundation.xcframework.zip"
-let checksum = "6249e8a497832025100e74b11357d6d6ff7b2dc8c2d9111b27e21a4680e44066"
+let url = "https://github.com/ridgelineinternational/wireguard-apple-xcframework/releases/download/0.0.1/WireGuardGoFoundation.xcframework.zip"
+let checksum = "3d9faeaacf8908a7e2d18bd4a314b89192e0afa974522379436d2edf7cf0c80f"
 
 let package = Package(
     name: "WireGuardKit",
